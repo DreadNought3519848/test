b137ae02a7169ce14fc3f1a14edadcfb2222e11f
Fukuhara <shouya070904@yahoo.co.jp>
2025-06-08 23:10:16 +0900

    headerメニューを変更

--- JAS/portfolioHOME.js
+++ JAS/portfolioHOME.js
@@ -1,13 +1,1 @@
 'use strict';
-const HeaderMenu=document.querySelectorAll('.Menu');
-
-HeaderMenu.forEach(menu=>{
-  menu.addEventListener('mouseenter',()=>{
-    menu.style.backgroundColor='black';
-    menu.style.color='white';
-  });
-  menu.addEventListener('mouseleave',()=>{
-    menu.style.backgroundColor='';
-    menu.style.color='';
-  });
-});
