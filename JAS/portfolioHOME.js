50ea11bdee9591a82b4962234ee79cd1e7b98d7e
Fukuhara <shouya070904@yahoo.co.jp>
2025-06-10 23:26:12 +0900

    ローディング画面完成

--- JAS/portfolioHOME.js
+++ JAS/portfolioHOME.js
@@ -2,5 +2,16 @@
 const Loading=document.querySelector('#Loading');
 
 window.addEventListener('load',()=>{
-
+  Loading.animate(
+    {
+      opacity:[1,0],
+      visibility:'hidden',
+    },
+    {
+      duration:2000,
+      delay:1200,
+      easing:'ease',
+      fill:'forwards',
+    }
+  );
 });
