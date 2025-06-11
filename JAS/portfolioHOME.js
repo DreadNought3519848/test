21eee9abf21b69407ae4f852b727d3f9873ade2d
Fukuhara <shouya070904@yahoo.co.jp>
2025-06-11 23:11:32 +0900

    ローディング画面ver2.0

--- JAS/portfolioHOME.js
+++ JAS/portfolioHOME.js
@@ -1,5 +1,6 @@
 'use strict';
 const Loading=document.querySelector('#Loading');
+const Loading_screen=document.querySelector('#Loading-screen');
 
 window.addEventListener('load',()=>{
   Loading.animate(
@@ -10,6 +11,17 @@ window.addEventListener('load',()=>{
     {
       duration:2000,
       delay:1200,
+      easing:'ease',
+      fill:'forwards',
+    }
+  );
+  Loading_screen.animate(
+    {
+      translate:['0 100vh','0 0','0 -100vh']
+    },
+    {
+      duration:2000,
+      delay:800,
       easing:'ease',
       fill:'forwards',
     }
